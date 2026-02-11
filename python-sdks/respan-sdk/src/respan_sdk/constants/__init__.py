52dabcb4eefab6067b0269a7a72f37d0e070f4a2
Nightingalelyy <nightingalelyy@outlook.com>
2026-02-11 15:51:32 +0800

    DEV-6428 Replace keywordsai-sdk with respan-sdk in backend

--- python-sdks/respan-sdk/src/respan_sdk/constants/__init__.py
+++ python-sdks/respan-sdk/src/respan_sdk/constants/__init__.py
@@ -1,6 +1,20 @@
+from respan_sdk.constants.tracing_constants import (
+    RESPAN_TRACING_INGEST_ENDPOINT,
+    resolve_tracing_ingest_endpoint,
+)
+
 DEFAULT_EVAL_LLM_ENGINE = "gpt-4o-mini"
 EVAL_COST_FALLBACK = 0.001
 LLM_ENGINE_FIELD_NAME = "llm_engine"
 
 # Import constants for default values
 UTC_EPOCH = "1970-01-01 00:00:00"
+
+__all__ = [
+    "DEFAULT_EVAL_LLM_ENGINE",
+    "EVAL_COST_FALLBACK",
+    "LLM_ENGINE_FIELD_NAME",
+    "UTC_EPOCH",
+    "RESPAN_TRACING_INGEST_ENDPOINT",
+    "resolve_tracing_ingest_endpoint",
+]
