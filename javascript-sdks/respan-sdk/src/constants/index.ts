55a0251b97da7306e2c4ba5465f7efa43bb6f2e6
Nightingalelyy <nightingalelyy@outlook.com>
2026-02-11 13:29:53 +0800

    DEV-5294 Implement AI-SDK response API tracing

--- javascript-sdks/respan-sdk/src/constants/index.ts
+++ javascript-sdks/respan-sdk/src/constants/index.ts
@@ -1,4 +1,3 @@
 export * from "./logConstants.js";
 export * from "./datasetConstants.js";
 export * from "./promptConstants.js";
-export * from "./tracingConstants.js";
--- javascript-sdks/respan-sdk/src/constants/tracingConstants.ts
+++
@@ -1,1 +0,0 @@
-export const RESPAN_INGEST_URL = "https://api.respan.ai/api/v1/traces/ingest";
