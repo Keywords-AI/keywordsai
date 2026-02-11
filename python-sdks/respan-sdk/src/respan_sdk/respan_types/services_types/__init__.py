68bbab0abc0c5aa9579e2c7b9d0dded0dd269f12
Nightingalelyy <nightingalelyy@outlook.com>
2026-02-11 11:21:23 +0800

    DEV-6248 Replace keywordsai-sdk with respan-sdk in backend

---
+++ python-sdks/respan-sdk/src/respan_sdk/respan_types/services_types/__init__.py
@@ -0,0 +1,11 @@
+from .hyperspell_types import (
+    HyperspellAddMemoryParams,
+    HyperspellSearchMemoriesParams,
+    HyperspellParams,
+)
+
+__all__ = [
+    "HyperspellAddMemoryParams",
+    "HyperspellSearchMemoriesParams",
+    "HyperspellParams",
+]
